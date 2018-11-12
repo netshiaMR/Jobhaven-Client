88b736048b07ab2839e09f60f76697a708c02c5d
netshiaMR <netshiarendani@gmail.com>
2018-11-12 21:46:05 +0200

    First Jobhaven Client Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { UpdateAccountComponent } from './update-account/update-account.component';
+import { LoginComponent } from './login/login.component';
+import { ApplyComponent } from './apply/apply.component';
+import { PostJobComponent } from './post-job/post-job.component';
+import { SearchComponent } from './search/search.component';
+import { SaveJobComponent } from './save-job/save-job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAccountComponent,
+    UpdateAccountComponent,
+    LoginComponent,
+    ApplyComponent,
+    PostJobComponent,
+    SearchComponent,
+    SaveJobComponent
   ],
   imports: [
     BrowserModule
